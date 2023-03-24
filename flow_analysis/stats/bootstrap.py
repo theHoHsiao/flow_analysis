354b0f8cd9277fa12e0778db41bc8b295907cf22
Ed Bennett <e.j.bennett@swansea.ac.uk>
2023-03-24 00:40:29 +0000

    allow collections of 1D data to be bootstrapped

--- flow_analysis/stats/bootstrap.py
+++ flow_analysis/stats/bootstrap.py
@@ -11,12 +11,16 @@ BOOTSTRAP_SAMPLE_COUNT = 200
 DEFAULT_RNG = default_rng()
 
 
+def bootstrap_finalize(samples):
+    return ufloat(mean(samples), std(samples))
+
+
 def basic_bootstrap(values, rng=DEFAULT_RNG):
     values = asarray(values)
     samples = []
     for _ in range(BOOTSTRAP_SAMPLE_COUNT):
         samples.append(mean(rng.choice(values, len(values))))
-    return ufloat(mean(samples, axis=0), std(samples))
+    return bootstrap_finalize(samples)
 
 
 def bootstrap_susceptibility(values, rng=DEFAULT_RNG):
@@ -25,4 +29,18 @@ def bootstrap_susceptibility(values, rng=DEFAULT_RNG):
     for _ in range(BOOTSTRAP_SAMPLE_COUNT):
         current_sample = rng.choice(values, len(values))
         samples.append(mean(current_sample ** 2) - mean(current_sample) ** 2)
-    return ufloat(mean(samples, axis=0), std(samples))
+    return bootstrap_finalize(samples)
+
+
+def sample_bootstrap_1d(values, rng=DEFAULT_RNG):
+    values = asarray(values)
+    bootstrap_sample_configurations = rng.integers(
+        values.shape[0],
+        size=(BOOTSTRAP_SAMPLE_COUNT, values.shape[0])
+    )
+    bootstrap_samples = empty((BOOTSTRAP_SAMPLE_COUNT, values.shape[1]))
+    for t_index in range(values.shape[1]):
+        bootstrap_samples[:, t_index] = values[
+            bootstrap_sample_configurations, t_index
+        ].mean(axis=1)
+    return asarray(bootstrap_samples)
