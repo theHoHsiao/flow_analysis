915794e417b589c28d6e0014010ffbbb2a7d93c0
Ed Bennett <e.j.bennett@swansea.ac.uk>
2024-07-12 15:53:25 +0100

    be able to read hasenfratz-petersen output files

--- flow_analysis/readers/__init__.py
+++ flow_analysis/readers/__init__.py
@@ -2,9 +2,11 @@
 
 from .read_grid import read_flows_grid
 from .read_hirep import read_flows_hirep
+from .read_hp import read_flows_hp
 
 
 readers = {
     "hirep": read_flows_hirep,
     "grid": read_flows_grid,
+    "hp": read_flows_hp,
 }
---
+++ flow_analysis/readers/read_hp.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from functools import lru_cache
+from re import match
+
+from ..flow import FlowStep, Flow, FlowEnsemble
+
+
+@lru_cache(maxsize=8)
+def read_flows_hp(filename):
+    flows = FlowEnsemble(filename)
+    flow = None
+    previous_line_trajectory = None
+    previous_line_flow_time = None
+
+    with open(filename) as f:
+        for line in f.readlines():
+            line_contents = line.split()
+            trajectory = int(line_contents[0])
+            flow_time, Ep, Ec = map(float, line_contents[1:])
+
+            if previous_line_flow_time is None or flow_time < previous_line_flow_time:
+                if previous_line_trajectory and previous_line_trajectory != trajectory - 1:
+                    raise ValueError("Configuration indices don't increment nicely.")
+                if flow:
+                    flows.append(flow)
+
+                flow = Flow(trajectory=trajectory)
+                previous_line_flow_time = 0
+
+            flow.append(FlowStep(flow_time, Ep, Ec, None))
+            previous_line_trajectory = trajectory
+            previous_line_flow_time = flow_time
+
+    flows.append(flow)
+    flows.freeze()
+    return flows
